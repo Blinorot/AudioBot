481e5dacf2ff6fff3043af6ddbde7dfe7942ca94
Blinorot <pmgrin.work@gmail.com>
2024-10-17 12:46:54 +0200

    Enhance code

--- src/streaming.py
+++ src/streaming.py
@@ -9,13 +9,21 @@ from torchaudio.io import StreamReader, StreamWriter
 
 from src.handlers import KWS_URL, full_handler
 
+SIG_START = 1
+SIG_STOP = 0
+KWS_THRESHOLD = 0.7
+SILENCE_PROPORTION_IN_CHUNK = 0.5
+IS_SILENCE = 1
+IS_SPEECH = 0
+
 
 def vad_checking(chunk, sample_rate):
     vad_chunk = vad(chunk, sample_rate=sample_rate)
-    if vad_chunk.shape[1] <= chunk.shape[1] * 0.5:
-        return 1
+    # some rule to identiy
+    if vad_chunk.shape[1] <= chunk.shape[1] * SILENCE_PROPORTION_IN_CHUNK:
+        return IS_SILENCE
     else:
-        return 0
+        return IS_SPEECH
 
 
 def get_kws():
@@ -29,21 +37,26 @@ def get_kws():
 
 
 def audio_stream(
-    init_queue: mp.Queue, queue: mp.Queue, source, format, chunk_size, sample_rate
+    init_queue: mp.Queue,
+    queue: mp.Queue,
+    source,
+    format,
+    chunk_size,
+    sample_rate,
+    vad_limit,
 ):
     kws = get_kws()
     streamer = StreamReader(src=source, format=format)
-    while init_queue.get() == 1:  # main process sent the signal to start
+    while init_queue.get() == SIG_START:  # main process sent the signal to start
         streamer.add_basic_audio_stream(
             frames_per_chunk=chunk_size, sample_rate=sample_rate
         )
         stream_iterator = streamer.stream(timeout=-1, backoff=1.0)
 
         vad_counts = 0
-        user_talked = 0
-        vad_limit = 3
+        user_talked = False
 
-        query_started = 0
+        query_started = False
 
         print("Start audio streaming")
         while True:
@@ -56,9 +69,9 @@ def audio_stream(
                 # check that the query is started
                 with torch.inference_mode():
                     keyword_proba = kws(chunk_data)
-                if keyword_proba > 0.7:
-                    print("Keyword Detected")
-                    query_started = 1
+                if keyword_proba > KWS_THRESHOLD:
+                    print(f"Keyword Detected: {keyword_proba}")
+                    query_started = True
                 else:
                     continue
 
@@ -68,14 +81,14 @@ def audio_stream(
                 vad_counts += 1
             else:
                 vad_counts = 0
-                user_talked = 1
+                user_talked = True
             if user_talked and vad_counts >= vad_limit:
                 break
             # if vad_counts > 5:
             #    break
-        queue.put(0)  # stop criteria for main process loop
+        queue.put(SIG_STOP)  # stop criteria for main process loop
         print("End of the user input")
-        streamer.remove_stream(0)
+        streamer.remove_stream(0)  # remove stream with id=0
 
 
 def init_stream(all_models, history, config):
@@ -92,6 +105,7 @@ def init_stream(all_models, history, config):
             config.stream_reader.format,
             config.stream_reader.chunk_size,
             config.stream_reader.sample_rate,
+            config.stream_reader.vad_limit,
         ),
     )
 
@@ -106,7 +120,7 @@ def init_stream(all_models, history, config):
     stream_writer.open()
 
     streaming_process.start()
-    stream_init_queue.put(1)  # sent signal to start
+    stream_init_queue.put(SIG_START)  # sent signal to start
     while True:
         try:
             while True:
@@ -141,14 +155,14 @@ def init_stream(all_models, history, config):
 
             # init next user input
             user_chunks = []
-            stream_init_queue.put(1)
+            stream_init_queue.put(SIG_START)
         except KeyboardInterrupt:
             break
         except Exception as exc:
             raise exc
 
     # send signal to exit
-    stream_init_queue.put(0)
+    stream_init_queue.put(SIG_STOP)
     streaming_process.join()
     stream_writer.close()
 
